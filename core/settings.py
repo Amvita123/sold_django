fe2339b4ac9ddd74a99f920a7251943eba211a94
Amit <20kumaramit12@gmail.com>
2025-05-12 13:09:15 +0530

    csrf
    csrf

--- core/settings.py
+++ core/settings.py
@@ -57,10 +57,12 @@ INSTALLED_APPS = [
     'ratings',
     'cms',
     'others',
+    'corsheaders',
 
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
