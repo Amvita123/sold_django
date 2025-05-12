ee5e1249832301445b300455b4e8f7605acd3a00
Amit <20kumaramit12@gmail.com>
2025-05-12 12:50:32 +0530

    db change
    db change

--- core/settings.py
+++ core/settings.py
@@ -98,23 +98,23 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': os.getenv('DBNAME'),
-#         'USER': os.getenv('DBUSER'),
-#         'PASSWORD': os.getenv('DBPASSWORD'),
-#         'HOST': os.getenv('DBHOST'),
-#         'PORT': os.getenv('DBPORT'),
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('DBNAME'),
+        'USER': os.getenv('DBUSER'),
+        'PASSWORD': os.getenv('DBPASSWORD'),
+        'HOST': os.getenv('DBHOST'),
+        'PORT': os.getenv('DBPORT'),
+    }
+}
 
 AUTH_USER_MODEL = 'users.User'
 
